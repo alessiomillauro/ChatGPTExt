4055b23e06b645f71c4606a9d901c268ee2ca783
alessiomillauro <alessio.millauro@gmail.com>
2023-09-11 16:33:31 +0200

    remove openAI key

--- app/src/main/java/com/android/chatgptext/utils/Constants.kt
+++ app/src/main/java/com/android/chatgptext/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.android.chatgptext.utils
 
-const val openAIApiKey = "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"
+const val openAIApiKey = ""
 
 const val urlToImageAppIcon =
     "https://res.cloudinary.com/apideck/image/upload/v1672442492/marketplaces/ckhg56iu1mkpc0b66vj7fsj3o/listings/-4-ans_frontend_assets.images.poe.app_icon.png-26-8aa0a2e5f237894d_tbragv.png"
